917039c73526f119e63a39a9c408f9f97ae13d99
andrew lupton <aflupton@gmail.com>
2018-04-30 16:41:52 -0700

    add changes

---
+++ WorldData/Models/WorldData.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using WorldData;
+using System;
+
+namespace WorldData.Models
+{
+  public class Data
+  {
+    //private variables
+
+    //constructor
+
+    //getter and setters
+
+    //additional methods
+  }
+}
